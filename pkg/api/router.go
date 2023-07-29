338b31add1505da04f9478253237c198f16e78b5
Rohith Varma Buddaraju <rohith.varma.buddaraju@gmail.com>
2023-07-29 16:12:39 +0530

    Add views

--- pkg/api/router.go
+++ pkg/api/router.go
@@ -4,15 +4,17 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/rohithvarma3000/mvc-lecture/pkg/controller"
 )
 
 func Start() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", ping).Methods("GET")
+	r.HandleFunc("/", controller.Welcome).Methods("GET")
+	r.HandleFunc("/add", controller.Add).Methods("POST")
 
 	http.ListenAndServe(":8000", r)
 }
 
-func ping(writer http.ResponseWriter, request *http.Request) {
-	writer.WriteHeader(http.StatusOK)
-}
+// func ping(writer http.ResponseWriter, request *http.Request) {
+// 	writer.WriteHeader(http.StatusOK)
+// }
--- pkg/controller/welcome.go
+++ pkg/controller/welcome.go
@@ -1,5 +1,13 @@
 package controller
 
-func Welcome() {
+import (
+	"net/http"
 
+	"github.com/rohithvarma3000/mvc-lecture/pkg/views"
+)
+
+func Welcome(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+	t := views.StartPage()
+	t.Execute(writer, nil)
 }
